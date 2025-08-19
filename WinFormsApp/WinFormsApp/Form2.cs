870c4bcdca8c0d86b28a6e3efec21c8f4bb89c59
sr_yz <dori31extend@gmail.com>
2025-08-20 00:01:43 +0900

    [update] 更新

--- WinFormsApp/WinFormsApp/Form2.cs
+++ WinFormsApp/WinFormsApp/Form2.cs
@@ -19,6 +19,7 @@ namespace WinFormsApp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //閉じる
             Close();
         }
     }
--- WinFormsApp/WinFormsApp/obj/Debug/net9.0-windows/WinFormsApp.AssemblyInfo.cs
+++ WinFormsApp/WinFormsApp/obj/Debug/net9.0-windows/WinFormsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcfdab7b9930f5813435d1cea1240309dd98d13b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5580649330e469bd724961fd3102b30f202576e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
